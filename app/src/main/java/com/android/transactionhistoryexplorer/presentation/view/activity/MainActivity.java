f0cac45608e1fc670eb93655864cfb00bdec6722
tuannguyen <dev.tuannguyen@gmail.com>
2026-03-26 18:58:58 +0900

    - Config build.gradle + Hilt + Room + Paging3

--- app/src/main/java/com/android/transactionhistoryexplorer/presentation/view/activity/MainActivity.java
+++ app/src/main/java/com/android/transactionhistoryexplorer/presentation/view/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.android.transactionhistoryexplorer;
+package com.android.transactionhistoryexplorer.presentation.view.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.transactionhistoryexplorer.R;
 
 public class MainActivity extends AppCompatActivity {
 
